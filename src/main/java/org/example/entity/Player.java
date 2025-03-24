42af883caa5298e4a9815701e3250613cf2c76fe
a.armari <a.armari@we-com.it>
2025-03-24 15:07:30 +0100

    pupazzetto moves

--- src/main/java/org/example/entity/Player.java
+++ src/main/java/org/example/entity/Player.java
@@ -2,9 +2,7 @@ package org.example.entity;
 
 
 import org.example.constants.GameCon;
-import org.example.constants.GraphicCon;
 import org.example.constants.KeyCon;
-import org.example.core.KeyHandler;
 import org.example.entity.ext.Entity;
 
 import java.awt.*;
@@ -20,36 +18,11 @@ public class Player extends Entity {
         X = GameCon.defaultX;
         Y = GameCon.defaultY;
         speed = GameCon.playerSpeed;
+        direction = KeyCon.DOWN;
+        getImage();
+
     }
 
-    @Override
-    public void update(KeyHandler kh) {
 
-        if (kh.upPressed) {
-            this.Y -= GameCon.playerSpeed;
-            System.out.println(KeyCon.UP + KeyCon.PRESSED);
-        }
 
-        if (kh.downPressed) {
-            this.Y += GameCon.playerSpeed;
-            System.out.println(KeyCon.DOWN + KeyCon.PRESSED);
-        }
-
-        if (kh.leftPressed) {
-            this.X -= GameCon.playerSpeed;
-            System.out.println(KeyCon.LEFT + KeyCon.PRESSED);
-        }
-
-        if (kh.rightPressed) {
-            this.X += GameCon.playerSpeed;
-            System.out.println(KeyCon.RIGHT + KeyCon.PRESSED);
-        }
-    }
-
-    @Override
-    public void draw(Graphics2D g2) {
-        g2.setColor(Color.RED);
-
-        g2.fillRect(this.X, this.Y, GraphicCon.tileSize, GraphicCon.tileSize);
-    }
 }
--- src/main/java/org/example/entity/ext/Entity.java
+++ src/main/java/org/example/entity/ext/Entity.java
@@ -1,8 +1,15 @@
 package org.example.entity.ext;
 
+import org.example.constants.GameCon;
+import org.example.constants.GraphicCon;
+import org.example.constants.KeyCon;
 import org.example.core.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
 
 public abstract class Entity {
 
@@ -11,19 +18,82 @@ public abstract class Entity {
 
     public Integer speed;
 
+    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    public String direction;
+
     public Entity() {
     }
 
-    public Entity(Integer x, Integer y, Integer speed) {
-        X = x;
-        Y = y;
-        this.speed = speed;
+    public void getImage() {
+        up1 = getPathUri("up1");
+        up2 = getPathUri("up2");
+        down1 = getPathUri("down1");
+        down2 = getPathUri("down2");
+        left1 = getPathUri("left1");
+        left2 = getPathUri("left2");
+        right1 = getPathUri("right1");
+        right2 = getPathUri("right2");
+
     }
 
-    public void update() {
+    private BufferedImage getPathUri(String imgName) {
+        String returnString = String.format("/%s/%s.png", this.getClass().getSimpleName(), imgName);
+
+        try {
+
+            return ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(returnString)));
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
     }
 
-    public abstract void update(KeyHandler kh);
+    public void update(KeyHandler kh) {
+        if (kh.upPressed) {
+            this.Y -= GameCon.playerSpeed;
+            this.direction = KeyCon.UP;
+            System.out.println(KeyCon.UP + KeyCon.PRESSED);
+        }
 
-    public abstract void draw(Graphics2D g2);
+        if (kh.downPressed) {
+            this.Y += GameCon.playerSpeed;
+            this.direction = KeyCon.DOWN;
+            System.out.println(KeyCon.DOWN + KeyCon.PRESSED);
+        }
+
+        if (kh.leftPressed) {
+            this.X -= GameCon.playerSpeed;
+            this.direction = KeyCon.LEFT;
+            System.out.println(KeyCon.LEFT + KeyCon.PRESSED);
+        }
+
+        if (kh.rightPressed) {
+            this.X += GameCon.playerSpeed;
+            this.direction = KeyCon.RIGHT;
+            System.out.println(KeyCon.RIGHT + KeyCon.PRESSED);
+        }
+    }
+
+    ;
+
+    public void draw(Graphics2D g2) {
+
+        //g2.setColor(Color.RED);
+        //g2.fillRect(this.X, this.Y, GraphicCon.tileSize, GraphicCon.tileSize);
+
+        BufferedImage image = null;
+
+        switch (direction) {
+            case KeyCon.UP -> image = up1;
+            case KeyCon.DOWN -> image = down1;
+            case KeyCon.LEFT -> image = left1;
+            case KeyCon.RIGHT -> image = right1;
+            default -> {}
+        }
+
+        g2.drawImage(image, X, Y, GraphicCon.tileSize, GraphicCon.tileSize, null);
+    }
+
 }
