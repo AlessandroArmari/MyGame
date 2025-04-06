08ba4cf3e53f1b15038f235f5c400e60050672de
hobisognodiunarma <alessandroarmari22@gmail.com>
2025-04-06 19:04:20 +0200

    togliere oppio ciclo

--- src/main/java/org/example/entity/tile/TileManager.java
+++ src/main/java/org/example/entity/tile/TileManager.java
@@ -1,5 +1,6 @@
 package org.example.entity.tile;
 
+import org.example.constants.GameCon;
 import org.example.constants.GraphicCon;
 import org.example.constants.TileCon;
 import org.example.core.GamePanel;
@@ -13,17 +14,20 @@ public class TileManager {
 
     public TileManager(GamePanel gp) {
         this.gp = gp;
-
-
     }
 
     public void drawImage(Graphics2D g2) {
 
-        for (int i = 0; i <= 1; i++) {
-            g2.drawImage(TileCon.roseField().image, GraphicCon.tileSize*i, 0, GraphicCon.tileSize, GraphicCon.tileSize, null);
+        for (int i = 0; i <= GraphicCon.maxScreenColumn; i++) {
+            for (int j = 0; j <= GraphicCon.maxScreenRow; j++) {
+                g2.drawImage(TileCon.roseField().image,
+                        GraphicCon.tileSize * i,
+                        GraphicCon.tileSize * j,
+                        GraphicCon.tileSize,
+                        GraphicCon.tileSize,
+                        null);
+            }
         }
-
-
     }
 
 
